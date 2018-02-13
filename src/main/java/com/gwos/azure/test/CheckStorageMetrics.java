1a18afd0862692f89e2668c80a45478ae65b9b14
Justin Chen <justin.chen@viridisio.com>
2018-02-13 12:04:36 -0800

    Add Junit 4.12 dependency and StorageAccount Metrics

---
+++ src/main/java/com/gwos/azure/test/CheckStorageMetrics.java
@@ -0,0 +1,90 @@
+package com.gwos.azure.test;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import com.gwos.azure.utils.VMMetricsUtils;
+import com.microsoft.azure.AzureEnvironment;
+import com.microsoft.azure.PagedList;
+import com.microsoft.azure.credentials.ApplicationTokenCredentials;
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.cosmosdb.CosmosDBAccount;
+import com.microsoft.azure.management.monitor.Metric;
+import com.microsoft.azure.management.monitor.MetricCollection;
+import com.microsoft.azure.management.monitor.MetricDefinition;
+import com.microsoft.azure.management.monitor.MetricDefinitions;
+import com.microsoft.azure.management.monitor.ResultType;
+import com.microsoft.azure.management.monitor.TimeSeriesElement;
+import com.microsoft.azure.management.storage.StorageAccount;
+import com.microsoft.rest.LogLevel;
+
+public class CheckStorageMetrics {
+	public static boolean runMetrics02(Azure azure) {		
+        //DateTime recordDateTime = DateTime.parse("2018-01-24T00:07:40.350Z");
+		DateTime recordDateTime = DateTime.now();
+		
+		try {
+			// =============================================================
+			// List virtual machines in the resource group
+			String resourceGroupName = "cloud-shell-storage-westus"; // Storage account is under this group
+			
+			System.out.println("----------------");
+			PagedList<StorageAccount> storageActs = azure.storageAccounts().listByResourceGroup(resourceGroupName);
+			for (StorageAccount storageAct : storageActs) {
+				List<MetricDefinition> listMetricDefinitionmds = azure.metricDefinitions().listByResource(storageAct.id());
+				for (MetricDefinition metricDefinition : listMetricDefinitionmds) {					
+			        // Query resource metrics
+			        MetricCollection metricCollection = metricDefinition.defineQuery()
+			                .startingFrom(recordDateTime.minusMinutes(10))	// last 10 minutes
+			                .endsBefore(recordDateTime)
+                            //.withAggregation("Average")
+                            //.withInterval(Period.minutes(5))				// Commented to use 1 minutes interval by default
+			                .withResultType(ResultType.DATA)
+			                .execute();
+
+			        VMMetricsUtils.printMetricCollection(metricCollection, storageAct.id());
+				}
+				
+			}
+
+			return true;
+		} catch (Exception f) {
+			f.printStackTrace();
+		} finally {
+			// TODO: nothing here
+		}
+		return false;
+	}
+		
+	public static void main(String[] args) {
+		try {
+			// https://docs.microsoft.com/en-us/java/azure/java-sdk-azure-authenticate
+			ApplicationTokenCredentials credentials = new ApplicationTokenCredentials(
+					"{Client Id}", 	
+			        "{Tenant Id}",	
+			        "{Client Secret}", 	
+			        AzureEnvironment.AZURE);
+
+			Azure azure = Azure
+			        .configure()
+			        .withLogLevel(LogLevel.BASIC)
+			        .authenticate(credentials)
+			        .withDefaultSubscription();
+			/*
+			// =============================================================
+			// Authenticate by a generated azure auth file configured in env. variable
+			final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+
+			Azure azure = Azure.configure().withLogLevel(LogLevel.BASIC).authenticate(credFile).withDefaultSubscription();
+			*/
+			// Print selected subscription
+			System.out.println("Selected subscription: " + azure.subscriptionId());
+			runMetrics02(azure);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
